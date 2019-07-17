98129e756f10afa9a776528fb93e62e400b7cca7
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2019-07-17 22:36:04 +0200

    Added ef migration for Payment removal

---
+++ Migrations/20190717203321_PaymentRemoved.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PozyczkoPrzypominajkaV2.Migrations
+{
+    public partial class PaymentRemoved : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Payments");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Payments",
+                columns: table => new
+                {
+                    PaymentID = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Amount = table.Column<decimal>(nullable: false),
+                    IsPaid = table.Column<bool>(nullable: false),
+                    LoanID = table.Column<int>(nullable: false),
+                    PlannedPaymentDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payments", x => x.PaymentID);
+                    table.ForeignKey(
+                        name: "FK_Payments_Loans_LoanID",
+                        column: x => x.LoanID,
+                        principalTable: "Loans",
+                        principalColumn: "LoanID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_LoanID",
+                table: "Payments",
+                column: "LoanID");
+        }
+    }
+}
