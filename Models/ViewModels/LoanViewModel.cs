b7ec86716d0a8f3e04a0def074c457bfa7ebb4e4
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2019-07-17 17:18:47 +0200

    Added LoanViewModel, and used on loan Index page.

---
+++ Models/ViewModels/LoanViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PozyczkoPrzypominajkaV2.Models.ViewModels
+{
+	public class LoanViewModel
+	{
+		public int LoanID { get; set; }
+
+		[Display(Name = "Udzielenie"), DataType(DataType.Date), DisplayFormat(DataFormatString = "yyyy.MM.dd")]
+		public DateTime Date { get; set; }
+
+		[Display(Name = "Udzielający"), DataType(DataType.Text)]
+		public string Giver { get; set; }
+
+		[Display(Name = "Biorący"), DataType(DataType.Text)]
+		public string Receiver { get; set; }
+
+		[Display(Name = "Pożyczone"), DataType(DataType.Currency), DisplayFormat(DataFormatString = "C0")]
+		public decimal Amount { get; set; }
+
+		[Display(Name = "Spłata"), DataType(DataType.Date), DisplayFormat(DataFormatString = "yyyy.MM.dd")]
+		public DateTime RepaymentDate { get; set; }
+
+		[Display(Name = "Do oddania"), DataType(DataType.Date), DisplayFormat(DataFormatString = "yyyy.MM.dd")]
+		public decimal RepaymentAmount { get; set; }
+
+		[Display(Name = "Oprocentowanie"), DisplayFormat(DataFormatString = "P0")]
+		public double Interest { get; set; }
+	}
+}
--- Pages/Loans/Index.cshtml.cs
+++ Pages/Loans/Index.cshtml.cs
@@ -2,28 +2,49 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using PozyczkoPrzypominajka.Models;
 using PozyczkoPrzypominajkaV2.Data;
+using PozyczkoPrzypominajkaV2.Models.ViewModels;
 
 namespace PozyczkoPrzypominajkaV2.Pages.Loans
 {
-    public class IndexModel : PageModel
-    {
-        private readonly PozyczkoPrzypominajkaV2.Data.ApplicationDbContext _context;
+	public class IndexModel : PageModel
+	{
+		private readonly ApplicationDbContext _context;
+		private readonly UserManager<AppUser> _userManager;
 
-        public IndexModel(PozyczkoPrzypominajkaV2.Data.ApplicationDbContext context)
-        {
-            _context = context;
-        }
+		public IndexModel(ApplicationDbContext context, UserManager<AppUser> userManager)
+		{
+			_context = context;
+			_userManager = userManager;
+		}
 
-        public IList<Loan> Loan { get;set; }
+		public IList<LoanViewModel> Loans { get; set; }
 
-        public async Task OnGetAsync()
-        {
-            Loan = await _context.Loans.ToListAsync();
-        }
-    }
+		public async Task OnGetAsync()
+		{
+			var currentUser = await _userManager.GetUserAsync(User);
+
+			Loans = await _context.Loans
+				.Where(l => l.Giver.Id == currentUser.Id || l.Receiver.Id == currentUser.Id)
+				.Select(l => new LoanViewModel() {
+					LoanID = l.LoanID,
+					Date = l.Date,
+					Giver = l.Giver.Imie + " " + l.Giver.Nazwisko,
+					Receiver = l.Receiver.Imie + " " + l.Receiver.Nazwisko,
+					Amount = l.Amount,
+					RepaymentDate = l.RepaymentDate,
+					RepaymentAmount = l.RepaymentAmount,
+					Interest = l.Interest
+				})
+				.AsNoTracking()
+				.ToListAsync();
+				
+		}
+	}
 }
