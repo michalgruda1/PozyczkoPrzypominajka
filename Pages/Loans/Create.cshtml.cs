d39fa3d81328a0daed7934a79a709c40639af2c8
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2019-07-19 15:10:45 +0200

    Added loan Giver dropdown in Create page.

--- Pages/Loans/Create.cshtml.cs
+++ Pages/Loans/Create.cshtml.cs
@@ -2,42 +2,71 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using PozyczkoPrzypominajka.Models;
 using PozyczkoPrzypominajkaV2.Data;
 
 namespace PozyczkoPrzypominajkaV2.Pages.Loans
 {
-    public class CreateModel : PageModel
-    {
-        private readonly PozyczkoPrzypominajkaV2.Data.ApplicationDbContext _context;
+	public class CreateModel : PageModel
+	{
+		public CreateModel(ApplicationDbContext context, UserManager<AppUser> userManager)
+		{
+			_context = context;
+			_userManager = userManager;
+		}
 
-        public CreateModel(PozyczkoPrzypominajkaV2.Data.ApplicationDbContext context)
-        {
-            _context = context;
-        }
+		private readonly ApplicationDbContext _context;
+		private readonly UserManager<AppUser> _userManager;
 
-        public IActionResult OnGet()
-        {
-            return Page();
-        }
+		[BindProperty] public Loan Loan { get; set; } = new Loan();
+		public SelectList Receivers { get; set; } = null;
 
-        [BindProperty]
-        public Loan Loan { get; set; }
+		public async Task<IActionResult> OnGetAsync()
+		{
+			var currentUser = await _userManager.GetUserAsync(User);
+			var Receivers = await InitReceivers(currentUser);
+			Loan.Giver = currentUser;
+			Loan.Date = DateTime.Now;
+			Loan.RepaymentDate = DateTime.Now + TimeSpan.FromDays(7);
 
-        public async Task<IActionResult> OnPostAsync()
-        {
-            if (!ModelState.IsValid)
-            {
-                return Page();
-            }
+			return Page();
+		}		
 
-            _context.Loans.Add(Loan);
-            await _context.SaveChangesAsync();
+		public async Task<IActionResult> OnPostAsync()
+		{
+			if (!ModelState.IsValid)
+			{
+				return Page();
+			}
 
-            return RedirectToPage("./Index");
-        }
-    }
+			_context.Loans.Add(Loan);
+			await _context.SaveChangesAsync();
+
+			return RedirectToPage("./Index");
+		}
+
+		private async Task<SelectList> InitReceivers(AppUser currentUser)
+		{
+			var receivers = await _context.Users
+				.Where(u => u.Id != currentUser.Id)
+				.Select(u =>
+					new
+					{
+						id = u.Id,
+						user = u.ToString()
+					})
+				.AsNoTracking()
+				.ToListAsync();
+
+			var ret = new SelectList(receivers, "id", "user");
+
+			return ret;
+		}
+
+	}
 }
