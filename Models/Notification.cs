988409e34e0a014f66b12ecc0081d8a064b72fd3
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2019-11-19 17:00:32 +0100

    Added constructor with parameters

--- Models/Notification.cs
+++ Models/Notification.cs
@@ -4,11 +4,21 @@ namespace PozyczkoPrzypominajka.Models
 {
 	public class Notification
 	{
-		public int NotificationID { get; set; }
+		public int? NotificationID { get; set; }
 		public int LoanID {get; set;}
 		public Loan Loan { get; set; }
 		public DateTime When { get; set; }
-		public string Text { get; set; }
+		public string Message { get; set; }
 		public ChannelEnum Channel { get; set; }
+
+		public Notification(int notificationID, int loanID, Loan loan, DateTime when, string message, ChannelEnum channel)
+		{
+			NotificationID = notificationID;
+			LoanID = loanID;
+			Loan = loan;
+			When = when;
+			Message = message;
+			Channel = channel;
+		}
 	}
 }
