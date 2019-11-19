8b7b488135c18c160ea23921d04af240d381f47a
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2019-11-19 16:35:56 +0100

    Revert "Unify private fields naming from _value to value"
    This reverts commit 4605b4f04d45234792e753711e7b71a29197c6a2.

--- Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -14,18 +14,18 @@ namespace PozyczkoPrzypominajkaV2.Areas.Identity.Pages.Account.Manage
 {
 	public partial class IndexModel : PageModel
 	{
-		private readonly UserManager<AppUser> userManager;
-		private readonly SignInManager<AppUser> signInManager;
-		private readonly IEmailSender emailSender;
+		private readonly UserManager<AppUser> _userManager;
+		private readonly SignInManager<AppUser> _signInManager;
+		private readonly IEmailSender _emailSender;
 
 		public IndexModel(
 				UserManager<AppUser> userManager,
 				SignInManager<AppUser> signInManager,
 				IEmailSender emailSender)
 		{
-			this.userManager = userManager;
-			this.signInManager = signInManager;
-			this.emailSender = emailSender;
+			_userManager = userManager;
+			_signInManager = signInManager;
+			_emailSender = emailSender;
 		}
 
 		public string Username { get; set; }
@@ -60,15 +60,15 @@ namespace PozyczkoPrzypominajkaV2.Areas.Identity.Pages.Account.Manage
 
 		public async Task<IActionResult> OnGetAsync()
 		{
-			var user = await userManager.GetUserAsync(User);
+			var user = await _userManager.GetUserAsync(User);
 			if (user == null)
 			{
-				return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
+				return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
 			}
 
-			var userName = await userManager.GetUserNameAsync(user);
-			var email = await userManager.GetEmailAsync(user);
-			var phoneNumber = await userManager.GetPhoneNumberAsync(user);
+			var userName = await _userManager.GetUserNameAsync(user);
+			var email = await _userManager.GetEmailAsync(user);
+			var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
 			var imie = user.Imie;
 			var nazwisko = user.Nazwisko;
 
@@ -82,7 +82,7 @@ namespace PozyczkoPrzypominajkaV2.Areas.Identity.Pages.Account.Manage
 				Nazwisko = nazwisko,
 			};
 
-			IsEmailConfirmed = await userManager.IsEmailConfirmedAsync(user);
+			IsEmailConfirmed = await _userManager.IsEmailConfirmedAsync(user);
 
 			return Page();
 		}
@@ -94,30 +94,30 @@ namespace PozyczkoPrzypominajkaV2.Areas.Identity.Pages.Account.Manage
 				return Page();
 			}
 
-			var user = await userManager.GetUserAsync(User);
+			var user = await _userManager.GetUserAsync(User);
 			if (user == null)
 			{
-				return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
+				return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
 			}
 
-			var email = await userManager.GetEmailAsync(user);
+			var email = await _userManager.GetEmailAsync(user);
 			if (Input.Email != email)
 			{
-				var setEmailResult = await userManager.SetEmailAsync(user, Input.Email);
+				var setEmailResult = await _userManager.SetEmailAsync(user, Input.Email);
 				if (!setEmailResult.Succeeded)
 				{
-					var userId = await userManager.GetUserIdAsync(user);
+					var userId = await _userManager.GetUserIdAsync(user);
 					throw new InvalidOperationException($"Unexpected error occurred setting email for user with ID '{userId}'.");
 				}
 			}
 
-			var phoneNumber = await userManager.GetPhoneNumberAsync(user);
+			var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
 			if (Input.PhoneNumber != phoneNumber)
 			{
-				var setPhoneResult = await userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
+				var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
 				if (!setPhoneResult.Succeeded)
 				{
-					var userId = await userManager.GetUserIdAsync(user);
+					var userId = await _userManager.GetUserIdAsync(user);
 					throw new InvalidOperationException($"Unexpected error occurred setting phone number for user with ID '{userId}'.");
 				}
 			}
@@ -132,9 +132,9 @@ namespace PozyczkoPrzypominajkaV2.Areas.Identity.Pages.Account.Manage
 				user.Nazwisko = Input.Nazwisko;
 			}
 
-			await userManager.UpdateAsync(user);
+			await _userManager.UpdateAsync(user);
 
-			await signInManager.RefreshSignInAsync(user);
+			await _signInManager.RefreshSignInAsync(user);
 			StatusMessage = "Your profile has been updated";
 			return RedirectToPage();
 		}
@@ -146,22 +146,22 @@ namespace PozyczkoPrzypominajkaV2.Areas.Identity.Pages.Account.Manage
 				return Page();
 			}
 
-			var user = await userManager.GetUserAsync(User);
+			var user = await _userManager.GetUserAsync(User);
 			if (user == null)
 			{
-				return NotFound($"Unable to load user with ID '{userManager.GetUserId(User)}'.");
+				return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
 			}
 
 
-			var userId = await userManager.GetUserIdAsync(user);
-			var email = await userManager.GetEmailAsync(user);
-			var code = await userManager.GenerateEmailConfirmationTokenAsync(user);
+			var userId = await _userManager.GetUserIdAsync(user);
+			var email = await _userManager.GetEmailAsync(user);
+			var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
 			var callbackUrl = Url.Page(
 					"/Account/ConfirmEmail",
 					pageHandler: null,
 					values: new { userId = userId, code = code },
 					protocol: Request.Scheme);
-			await emailSender.SendEmailAsync(
+			await _emailSender.SendEmailAsync(
 					email,
 					"Confirm your email",
 					$"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
