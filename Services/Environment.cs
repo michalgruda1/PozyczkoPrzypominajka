f0f29843da349d73a57cb4477aa14a41cb58ffdc
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2019-11-19 16:53:09 +0100

    Added Environment interface and service to provide testable Now()

---
+++ Services/Environment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PozyczkoPrzypominajkaV2.Services
+{
+	public class Environment
+	{
+		private DateTime NowDateTime = DateTime.Now;
+
+		public void SetNow(DateTime SomeTime)
+		{
+			this.NowDateTime = SomeTime;
+		}
+
+		public DateTime Now()
+		{
+			return this.NowDateTime;
+		}
+	}
+}
