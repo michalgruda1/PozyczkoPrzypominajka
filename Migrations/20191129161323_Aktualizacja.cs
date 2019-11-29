2b0b790ec0943048e92998bb445b1368dc06e579
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2019-11-29 18:23:32 +0100

    Minor migration  added - one field name changed

---
+++ Migrations/20191129161323_Aktualizacja.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PozyczkoPrzypominajkaV2.Migrations
+{
+    public partial class Aktualizacja : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Text",
+                table: "Notifications",
+                newName: "Message");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Message",
+                table: "Notifications",
+                newName: "Text");
+        }
+    }
+}
