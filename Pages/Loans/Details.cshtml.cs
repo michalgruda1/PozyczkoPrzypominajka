6ca93ec11ee5dacb0e027d67fd2454406a827e61
Michal Gruda <42464732+michalgruda1@users.noreply.github.com>
2019-12-09 12:08:31 +0100

    Fixed usings

--- Pages/Loans/Details.cshtml.cs
+++ Pages/Loans/Details.cshtml.cs
@@ -1,40 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
-using PozyczkoPrzypominajka.Models;
-using PozyczkoPrzypominajkaV2.Data;
+using PozyczkoPrzypominajkaV2.Models.Loan;
+using System.Threading.Tasks;
 
 namespace PozyczkoPrzypominajkaV2.Pages.Loans
 {
-    public class DetailsModel : PageModel
+  public class DetailsModel : PageModel
+  {
+    private readonly PozyczkoPrzypominajkaV2.Data.ApplicationDbContext _context;
+
+    public DetailsModel(PozyczkoPrzypominajkaV2.Data.ApplicationDbContext context)
     {
-        private readonly PozyczkoPrzypominajkaV2.Data.ApplicationDbContext _context;
-
-        public DetailsModel(PozyczkoPrzypominajkaV2.Data.ApplicationDbContext context)
-        {
-            _context = context;
-        }
-
-        public Loan Loan { get; set; }
-
-        public async Task<IActionResult> OnGetAsync(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            Loan = await _context.Loans.FirstOrDefaultAsync(m => m.LoanID == id);
-
-            if (Loan == null)
-            {
-                return NotFound();
-            }
-            return Page();
-        }
+      _context = context;
     }
+
+    public Loan Loan { get; set; }
+
+    public async Task<IActionResult> OnGetAsync(int? id)
+    {
+      if (id == null)
+      {
+        return NotFound();
+      }
+
+      Loan = await _context.Loans.FirstOrDefaultAsync(m => m.LoanID == id);
+
+      if (Loan == null)
+      {
+        return NotFound();
+      }
+      return Page();
+    }
+  }
 }
